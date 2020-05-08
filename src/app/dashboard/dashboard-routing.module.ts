a9869044dc7c3ba4d364e8ddd13f97dde283ec86
aurghyadip <aurghya.blog@gmail.com>
2020-05-08 16:51:29 +0530

    added routing to dashboard

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
-
+const routes: Routes = [
+  {
+    path: 'main',
+    component: DashboardComponent,
+    children: []
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard/main',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class DashboardRoutingModule { }
