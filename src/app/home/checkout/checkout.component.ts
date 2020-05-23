860652aacfe6ac8f32f61242bc35f8dce7b36067
Aurghyadip <aurghya.blog@gmail.com>
2020-05-24 00:47:58 +0530

    added initializer

--- src/app/home/checkout/checkout.component.ts
+++ src/app/home/checkout/checkout.component.ts
@@ -1,3 +1,8 @@
+import { User } from 'src/app/models/auth/auth';
+import { OrderService } from './../../services/data/order.service';
+import { CoursesService } from './../../services/data/courses.service';
+import { UserService } from 'src/app/services/data/user.service';
+import { AuthService } from './../../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  localUser: any;
+  user: User;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+    private readonly courseService: CoursesService,
+    private readonly orderService: OrderService,
+  ) {
+    this.localUser = this.authService.getUser();
+  }
 
   ngOnInit(): void {
+    this.userService.getUser(this.localUser.id).subscribe(
+      (u: User) => {
+        this.user = u;
+        //TODO: fetch order details from params
+      }, (error: any) => console.log(error)
+    );
   }
 
 }
