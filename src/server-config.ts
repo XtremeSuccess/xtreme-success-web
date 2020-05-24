bc355651ad69570f12a692784f12a99acd36d5c7
Aurghyadip <aurghya.blog@gmail.com>
2020-05-25 00:57:18 +0530

    fixed code styling issues

--- src/server-config.ts
+++ src/server-config.ts
@@ -1,2 +1,2 @@
-export const domain: string = "localhost:1337"
-export const url: string = "http://localhost:1337"
+export const domain: string = "localhost:1337";
+export const url: string = "http://localhost:1337";
